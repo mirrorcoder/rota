3c17c99cb719e4abd72f7324052d9f059ca082c4
Dmitry Sozinov <mirrorcoder1992@gmail.com>
2017-02-25 01:20:41 +0400

    Init commint

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(function(){
+
+    function pjaxPageLoad(){
+        $('.widget').widgster();
+        $('#datetimefromorders').datetimepicker({
+            format: 'YYYY-MM-DD HH:mm'
+        });
+        $('#datetimetoorders').datetimepicker({
+            format: 'MM/DD/YYYY'
+        });
+        $('[data-toggle=tooltip]').tooltip();
+        $("#mask-size-cargo").inputmask({mask: "999x999x999"});
+        $("#time-load-from").inputmask({mask: "99:99"});
+        $("#time-load-to").inputmask({mask: "99:99"});
+        $("#time-load-break-from").inputmask({mask: "99:99"});
+        $("#time-load-break-to").inputmask({mask: "99:99"});
+        $("#time-unload-from").inputmask({mask: "99:99"});
+        $("#time-unload-to").inputmask({mask: "99:99"});
+        $("#time-unload-break-from").inputmask({mask: "99:99"});
+        $("#time-unload-break-to").inputmask({mask: "99:99"});
+        new Switchery(document.getElementById('checkbox-ios1'));
+    }
+
+    pjaxPageLoad();
+    SingApp.onPageLoad(pjaxPageLoad);
+
+});
